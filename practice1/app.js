2c9207902186240d3cd8ac541a8e9b6e06c1affd
KimInteger <kjs20151240@gmail.com>
2024-05-27 12:31:50 +0900

    res.end의 매개변수로 readFile로 읽어온 data를 사용

--- practice1/app.js
+++ practice1/app.js
@@ -15,6 +15,7 @@ const server = http.createServer((req,res)=>{
           res.end('서버 연결 오류');
         }
         res.writeHead(200, {"Content-Type":"text/html; charset=UTF-8"});
+        res.end(data);
       })
     }
   }
