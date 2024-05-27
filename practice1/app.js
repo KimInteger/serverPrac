fc9a5315979ac292137a5345d97db6008c6d5296
KimInteger <kjs20151240@gmail.com>
2024-05-27 15:31:22 +0900

    req.method가 GET이 아닐경우를 상정하여 else if로 POST일때의 조건을 추가 이후 else를 추가하여 나머지를 처리

--- practice1/app.js
+++ practice1/app.js
@@ -42,6 +42,10 @@ const server = http.createServer((req,res)=>{
       res.writeHead(404, {"Content-Type":"text/plain; charset=UTF-8"});
       res.end("페이지를 찾을 수 없습니다.");
     }
+  } else if (req.method === 'POST') {
+
+  } else {
+    
   }
 });
 
