170490ed7090363b1782bc243593d01fcf0a7f8b
KimInteger <kjs20151240@gmail.com>
2024-05-27 12:52:44 +0900

    encode된 데이터를 확인히기 위한 console.log를 작성

--- practice1/app.js
+++ practice1/app.js
@@ -20,6 +20,7 @@ const server = http.createServer((req,res)=>{
     } else if (req.url.startsWith('/get')) {
       let formData = req.url.split('?')[1];
       let encodeData = qs.parse(formData);
+      console.log(encodeData);
     } else {
       res.writeHead(404, {"Content-Type":"text/plain; charset=UTF-8"});
       res.end("페이지를 찾을 수 없습니다.");
