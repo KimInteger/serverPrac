b5c711b1da600605367a4be854692ec1c32e8f4b
KimInteger <kjs20151240@gmail.com>
2024-05-27 15:51:36 +0900

    res.end를 작성 페이지를 찾을 수 없습니다.

--- practice1/app.js
+++ practice1/app.js
@@ -71,6 +71,7 @@ const server = http.createServer((req,res)=>{
       });
     } else {
       res.writeHead(404,{"Content-Type":"text/plain; charset=UTF-8"});
+      res.end('페이지를 찾을 수 없습니다.');
     }
   } else {
     res.writeHead(404,{"Content-Type":"text/plain; charset=UTF-8"});
