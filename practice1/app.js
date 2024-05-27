9f56838b8f2f82a99a24b8168288e1ad43406099
KimInteger <kjs20151240@gmail.com>
2024-05-27 15:25:02 +0900

    fs모듈readFile매서드에 err조건문의 실행문 writeHead세팅

--- practice1/app.js
+++ practice1/app.js
@@ -32,7 +32,7 @@ const server = http.createServer((req,res)=>{
 
       fs.readFile('./public/index.html',(err,data)=>{
         if (err) {
-          
+          res.writeHead(500,{"Content-Type" : "text/plain; charset=UTF-8"});)
         }
       });
     } else {
