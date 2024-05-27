55ae8368d63eb3a75447e0e4d7ec506692b325ee
KimInteger <kjs20151240@gmail.com>
2024-05-27 12:05:04 +0900

    서버와 포트를 연결, 서버가 열리는지 확인하기위해 console.log를 사용

--- practice1/app.js
+++ practice1/app.js
@@ -7,5 +7,13 @@ const qs = require('node:querystring');
 const path = require('node:path');
 
 const server = http.createServer((req,res)=>{
-  
+  console.log(req.method);
 });
+
+server.listen(3000,(err)=>{
+  if(err){
+    console.error(err);
+  }
+  console.log("서버가동확인용");
+  console.log('http://localhost:3000');
+})
