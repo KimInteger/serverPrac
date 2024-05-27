7bac5069cfd80fa31ace3effa9611d85b3ff001f
KimInteger <kjs20151240@gmail.com>
2024-05-27 12:34:51 +0900

    req.method가 GET일 경우 조건을 모두 설정

--- practice1/app.js
+++ practice1/app.js
@@ -16,7 +16,9 @@ const server = http.createServer((req,res)=>{
         }
         res.writeHead(200, {"Content-Type":"text/html; charset=UTF-8"});
         res.end(data);
-      })
+      });
+    } else if (req.url.startsWith('/get')) {
+
     }
   }
 });
