1b7906a7cd4f0874500a47ebc162bc5d4a51e164
KimInteger <kjs20151240@gmail.com>
2024-05-27 13:07:06 +0900

    GET매서드로 받아온 form데이터로 파일을 생성하기위해 fs모듈의 writeFile매서드의 path,content,callback을 할당

--- practice1/app.js
+++ practice1/app.js
@@ -22,8 +22,10 @@ const server = http.createServer((req,res)=>{
       let encodeData = qs.parse(formData);
       const name = encodeData.name;
       const hobby = encodeData.hobby;
-      console.log(name);
-      console.log(hobby);
+
+      fs.writeFile(`./${name}.txt`,hobby,(err)=>{
+        
+      });
     } else {
       res.writeHead(404, {"Content-Type":"text/plain; charset=UTF-8"});
       res.end("페이지를 찾을 수 없습니다.");
