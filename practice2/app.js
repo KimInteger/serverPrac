784c2e4926d5c76cd4addc246d7ed1db5b753e8d
KimInteger <kjs20151240@gmail.com>
2024-05-28 10:32:49 +0900

    else if get에 /를 안붙였었음

--- practice2/app.js
+++ practice2/app.js
@@ -20,7 +20,7 @@ const server = http.createServer((req,res)=>{
         res.writeHead(200, {"Content-Type":"text/html; charset=UTF-8"});
         res.end(data);
       });
-    } else if (req.url === 'get') {
+    } else if (req.url === '/get') {
 
     } else {
 
