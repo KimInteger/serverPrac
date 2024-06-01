615491a9dbaf0bd48386d734cd4fc1d9951e6097
KimInteger <kjs20151240@gmail.com>
2024-06-01 22:49:46 +0900

    서버 생성하고 포트에 연결하여 열리는 것까지 확인

--- practice3/app.js
+++ practice3/app.js
@@ -5,3 +5,16 @@ const fs = require('node:fs');
 const qs = require('node:querystring');
 
 const path = require('node:path');
+
+const server = http.createServer((req,res)=>{
+  console.log('서버확인');
+});
+
+server.listen(3000,(err)=>{
+  if(err){
+    console.error(err);
+  } else {
+    console.log("서버가 잘 열려있습니다.");
+    console.log(`http://localhost:3000`);
+  }
+});
