e833c26aae5ae48c31b9a8c952fba27018b5ea90
KimInteger <kjs20151240@gmail.com>
2024-06-02 14:19:08 +0900

    server생성 및 PORT에 연결

--- practice4/app.js
+++ practice4/app.js
@@ -7,3 +7,16 @@ const qs = require('node:querystring');
 const path = require('node:path');
 
 const PORT = process.env.PORT || 8080;
+
+const server = http.createServer((req,res)=>{
+  console.log(1);
+});
+
+server.listen(PORT,(err)=>{
+  if (err) {
+    console.error('에러가 발생했당께요 : ', err);
+  } else {
+    console.log("서버가 열렸도르");
+    console.log(`http://localhost:${PORT}`);
+  }
+})
